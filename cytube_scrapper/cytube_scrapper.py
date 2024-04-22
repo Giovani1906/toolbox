29504442de938706a82155f9b3c7b8df3fa3889f
Giovani1906 <4cc.giovani1906@gmail.com>
2024-04-22 17:02:28 +0200

    Added handle executable check to the powershell script. Changed the behaviour for a channel on the Cytube Scrapper.

--- cytube_scrapper/cytube_scrapper.py
+++ cytube_scrapper/cytube_scrapper.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime
 import html
 import json
@@ -40,6 +41,15 @@ def yeast():
         return r
 
 
+def get_url(server_addr: str, eio_mode: str, _sid: str = None):
+    eio = 4 if eio_mode == "4cc" else 3
+    if not _sid:
+        return f"{server_addr}/socket.io/?EIO={eio}&transport=polling&t={yeast()}"
+    return (
+        f"{server_addr}/socket.io/?EIO={eio}&transport=polling&t={yeast()}&sid={_sid}"
+    )
+
+
 def ping(socket: websocket.WebSocketApp):
     while True:
         time.sleep(socket.ping_interval)
@@ -49,6 +59,8 @@ def ping(socket: websocket.WebSocketApp):
 def on_message(socket: websocket.WebSocketApp, message: str):
     if message == "3probe":
         socket.send("5")
+    elif message == "2" and mode == "4cc":
+        socket.send("3")
     else:
         if chat_message := re.match(
             r'42\["chatMsg",{"username":"(.*)","msg":"(.*)","meta":{.*},"time":(\d{13})}]',
@@ -56,7 +68,7 @@ def on_message(socket: websocket.WebSocketApp, message: str):
         ):
             username, message, timestamp = chat_message.groups()
             date = datetime.datetime.fromtimestamp(int(timestamp) / 1000.0)
-            output = "{0.hour:02d}:{0.minute:02d}:{0.second:02d}] {1}: {2}"
+            output = "[{0.hour:02d}:{0.minute:02d}:{0.second:02d}] {1}: {2}"
             if "span" in message:
                 message = message.split("span")[0][:-2]
                 print(output.format(date, username, html.unescape(message)))
@@ -69,56 +81,76 @@ def on_error(socket: websocket.WebSocketApp, error):
 
 
 def on_close(socket: websocket.WebSocketApp, close_status_code, close_msg):
-    print("on_close args:")
-    if close_status_code or close_msg:
+    print("Connection closed...")
+    if close_status_code:
         print(f"close status code: {close_status_code}")
+    if close_msg:
         print(f"close message: {close_msg}")
 
 
 def on_open(socket: websocket.WebSocketApp):
-    print("\n")
+    print("")
     socket.send("2probe")
-    threading.Thread(target=ping, args=(socket,)).start()
+    if mode != "4cc":
+        threading.Thread(target=ping, args=(socket,)).start()
 
 
 if __name__ == "__main__":
-    mode = "4cc"
+    try:
+        parser = argparse.ArgumentParser(prog="Cytube Scrapper")
+        parser.add_argument("mode", nargs="?", default="4cc", choices=["4cc", "vgl"])
+        mode = parser.parse_args().mode
 
-    cytube_url = {
-        "4cc": "https://cytu.be/socketconfig/the4chancup.json",
-        "vgl": "https://cytube.implying.fun/socketconfig/vgleague.json",
-    }
+        print(f"Cytube Scrapper\n\nRunning in {mode} mode.")
 
-    sesh = requests.Session()
-    resp = sesh.request("GET", cytube_url[mode])
-    server = json.loads(resp.text)["servers"][0]["url"]
+        cytube_url = {
+            "4cc": "https://cytu.be/socketconfig/the4chancup.json",
+            "vgl": "https://cytube.implying.fun/socketconfig/vgleague.json",
+        }
 
-    r_url = f"{server}/socket.io/?EIO=3&transport=polling&t={yeast()}"
-    _sid = sesh.request("GET", r_url)
-    sid = re.match(r'.*{"sid":"(.*)",.*', _sid.text).groups()[0]
+        sesh = requests.Session()
+        resp = sesh.request("GET", cytube_url[mode])
+        serv = json.loads(resp.text)["servers"][0]["url"]
 
-    r_url += f"&sid={sid}"
-    sesh.request("GET", r_url)
+        serv_info_raw = sesh.request("GET", get_url(serv, mode))
+        regex = re.compile(r"[\d\W]*({.*})")
+        serv_info = json.loads(regex.match(serv_info_raw.text).groups()[0])
+        sid = serv_info["sid"]
 
-    # websocket.enableTrace(True)
-    ws_kwargs = {
-        "url": f'wss://{server.split("//")[1]}/socket.io/?EIO=3&transport=websocket&sid={sid}',
-        "on_message": on_message,
-        "on_error": on_error,
-        "on_close": on_close,
-    }
-    ws = websocket.WebSocketApp(**ws_kwargs)
-    ws.on_open = on_open
+        if mode == "4cc":
+            sesh.request("POST", get_url(serv, mode, sid), data="40")
+        sesh.request("GET", get_url(serv, mode, sid))
 
-    thread_kwargs = {
-        "target": ws.run_forever,
-        "kwargs": {"ping_interval": 20, "ping_timeout": 5},
-    }
-    threading.Thread(**thread_kwargs).start()
+        # websocket.enableTrace(True)
+        ws_url = {
+            "4cc": f"wss://{serv.split("//")[1]}/socket.io/?EIO=4&transport=websocket&sid={sid}",
+            "vgl": f"wss://{serv.split("//")[1]}/socket.io/?EIO=3&transport=websocket&sid={sid}",
+        }
+        ws_kwargs = {
+            "url": ws_url[mode],
+            "on_message": on_message,
+            "on_error": on_error,
+            "on_close": on_close,
+        }
+        ws = websocket.WebSocketApp(**ws_kwargs)
+        ws.on_open = on_open
 
-    join_channel_data = {
-        "4cc": '40:42["joinChannel",{"name":"the4chancup"}]',
-        "vgl": '37:42["joinChannel",{"name":"vgleague"}]',
-    }
-    sesh.request("POST", r_url, data=join_channel_data[mode])
-    sesh.request("GET", r_url)
+        thread_kwargs = {
+            "target": ws.run_forever,
+            "kwargs": {
+                "ping_interval": serv_info["pingInterval"],
+                "ping_timeout": 10,
+            },
+        }
+        threading.Thread(**thread_kwargs).start()
+
+        join_channel_data = {
+            "4cc": '42["joinChannel",{"name":"the4chancup"}]',
+            "vgl": '37:42["joinChannel",{"name":"vgleague"}]',
+        }
+        sesh.request("POST", get_url(serv, mode, sid), data=join_channel_data[mode])
+        sesh.request("GET", get_url(serv, mode, sid))
+    except KeyboardInterrupt:
+        print("Exiting...")
+        time.sleep(1)
+        exit(0)
--- multi_pes/multi_pes.ps1
+++ multi_pes/multi_pes.ps1
@@ -1,12 +1,26 @@
 ﻿#Requires -RunAsAdministrator
+if (Test-Path -Path ".\handle.exe" -PathType Leaf) {
+    $executable = ".\handle.exe"
+}
+elseif (Test-Path -Path ".\handle\handle.exe" -PathType Leaf) {
+    $executable = ".\handle\handle.exe"
+}
+elseif (Test-Path -Path ".\multi_pes\handle\handle.exe" -PathType Leaf) {
+    $executable = ".\multi_pes\handle\handle.exe"
+}
+else {
+    Write-Output "Handle could not be found. Please install it next to the Powershell script.`nYou can get it from here: https://learn.microsoft.com/en-us/sysinternals/downloads/handle"
+    [System.Environment]::Exit(0)
+}
+
 Write-Output "A PowerShell script that allows a version of PES to be ran multiple times.`nPES can be launched again after a new line appears down bellow."
-while (1){
+while (1) {
     ForEach($pes_process in Get-Process -Name "PES*") {
         if($pes_process.mainWindowTitle -like "Pro Evolution Soccer*" -or "eFootball PES*") {
-            $handle = .\handle\handle.exe -p $pes_process.Id -a -nobanner "boot"
+            $handle = & $executable -p $pes_process.Id -a -nobanner "boot"
             if($handle -ne "No matching handles found.") {
                 if($handle -match ".*Mutant\s*(\w{2,}): \\.*\sBoot") {
-                    $mutex = .\handle\handle.exe -p $pes_process.Id -nobanner -c $Matches.Item(1) -y
+                    $mutex = & $executable -p $pes_process.Id -nobanner -c $Matches.Item(1) -y
                     if($mutex -eq "Handle closed.") {
                         Write-Output "`"$($pes_process.Name).exe (PID: $($pes_process.Id))`" has been modified to allow another window to be launched."
                     }
