a3738210d3ddf49ec3288f7eadcb38fb6383a3a1
Giovani1906 <4cc.giovani1906@gmail.com>
2024-05-29 11:02:50 +0200

    Added GNU/Linux support to ftex_convert.py Some variable and argument names were changed for an improved code appearance. Added logging on what the conversion does. Stopped using multiprocessing.Pool because it barely improves anything due to the reliance on external software.

--- ftex/ftex_convert.py
+++ ftex/ftex_convert.py
@@ -1,25 +1,23 @@
 import argparse
 import copy
 import os
-import subprocess
 import sys
-from functools import partial
-from multiprocessing import Pool
+from subprocess import PIPE, Popen, check_output
 
-from ftex_info import fmt_choices, ftex_check
+from ftex_info import fmt_choices, ftex_check, ftex_fmt_str, FtexHeader
 from pes_file_tools.ftex import dds_to_ftex_buffer, ftex_to_dds_buffer
 
 
 def check_and_convert(
     filename: str,
-    check_for_format: str,
-    check_for_version: float,
-    convert_to_format: str,
-    convert_to_version: float,
+    chk_fmt: str,
+    chk_ver: float,
+    conv_fmt: str,
+    conv_ver: float,
     dont_preserve_original: bool,
     keep_dds_file: bool,
     filedir: str = None,
-):
+) -> str | None:
     if filename.split(".")[-1].lower() != "ftex":
         return
 
@@ -32,33 +30,35 @@ def check_and_convert(
     with open(path, "rb") as input_buffer:
         buffer = input_buffer.read()
 
-    chk_fmt = [check_for_format] if check_for_format else []
-    if not (ftex := ftex_check(buffer, chk_fmt, check_for_version)):
+    chk_fmt = [chk_fmt] if chk_fmt else []
+    if not (ftex := ftex_check(buffer, chk_fmt, chk_ver)):
         return
 
-    if not convert_to_format and convert_to_version != round(ftex.version, 2):
+    if not conv_fmt and conv_ver != round(ftex.version, 2):
         ftex203 = b"\x85\xeb\x01@"
         ftex204 = b"\\\x8f\x02@"
-        match convert_to_version:
+        match conv_ver:
             case 2.03:
-                buffer = buffer.replace(ftex204, ftex203)
+                buffer_conv = buffer.replace(ftex204, ftex203)
             case 2.04:
-                buffer = buffer.replace(ftex203, ftex204)
+                buffer_conv = buffer.replace(ftex203, ftex204)
+            case _:
+                return
 
         if not dont_preserve_original:
             os.rename(path, path.replace(".ftex", "_old.ftex"))
 
         with open(path, "wb") as output_buffer:
-            output_buffer.write(buffer)
+            output_buffer.write(buffer_conv)
     else:
         dds_buffer = ftex_to_dds_buffer(buffer)
-        match convert_to_format:
+        match conv_fmt:
             case "BC1":
-                convert_to_format = "DXT1"
+                conv_fmt = "DXT1"
             case "BC2":
-                convert_to_format = "DXT3"
+                conv_fmt = "DXT3"
             case "BC3":
-                convert_to_format = "DXT5"
+                conv_fmt = "DXT5"
 
         if sys.platform == "win32":
             dds_path = path.replace(".ftex", "_tmp.dds")
@@ -68,13 +68,13 @@ def check_and_convert(
             cmd = [
                 os.path.join("bin", "texconv.exe"),
                 "-f",
-                convert_to_format,
+                conv_fmt,
                 "-y",
                 "-o",
                 filedir,
                 dds_path,
             ]
-            p_out = subprocess.check_output(cmd)
+            check_output(cmd)
 
             with open(dds_path, "rb") as dcf:
                 dds_converted_buffer = dcf.read()
@@ -87,81 +87,87 @@ def check_and_convert(
                 "-format",
                 "dds",
                 "-define",
-                f"dds:compression={convert_to_format.lower()}",
+                f"dds:compression={conv_fmt.lower()}",
                 "-",
                 "-",
             ]
-            # cmd = ["identify", "-format", "%m %[compression]", "-"]
-            proc = subprocess.Popen(
+            proc = Popen(
                 cmd,
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
+                stdin=PIPE,
+                stdout=PIPE,
+                stderr=PIPE,
             )
-            p_out, p_err = proc.communicate(dds_buffer)
+            dds_converted_buffer, p_err = proc.communicate(dds_buffer)
 
             if p_err:
                 raise Exception(p_err.decode("utf-8"))
-            print(p_out.decode("utf-8"))
-            return
 
         if not dont_preserve_original:
             os.rename(path, path.replace(".ftex", "_old.ftex"))
 
-        buffer = dds_to_ftex_buffer(dds_converted_buffer)
+        buffer_conv = dds_to_ftex_buffer(dds_converted_buffer)
 
-        if convert_to_version != round(ftex.version, 2):
+        if conv_ver != round(ftex.version, 2):
             ftex203 = b"\x85\xeb\x01@"
             ftex204 = b"\\\x8f\x02@"
-            match convert_to_version:
+            match conv_ver:
                 case 2.03:
-                    buffer = buffer.replace(ftex204, ftex203)
+                    buffer_conv = buffer_conv.replace(ftex204, ftex203)
                 case 2.04:
-                    buffer = buffer.replace(ftex203, ftex204)
+                    buffer_conv = buffer_conv.replace(ftex203, ftex204)
 
         with open(path, "wb") as output_buffer:
-            output_buffer.write(buffer)
+            output_buffer.write(buffer_conv)
+
+    ftex_conv = FtexHeader(buffer_conv)
+
+    return (
+        f"Converting: {filename}\n"
+        f"FTEX VERSION {round(ftex.version, 2)} "
+        f"FORMAT {ftex_fmt_str[ftex.pixel_fmt]} > "
+        f"FTEX VERSION {round(ftex_conv.version, 2)} "
+        f"FORMAT {ftex_fmt_str[ftex_conv.pixel_fmt]}"
+    )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="FTEX Mass Converter")
     parser.add_argument("path")
-    parser.add_argument("--check-for-format", choices=fmt_choices)
-    parser.add_argument("--convert-to-format", choices=fmt_choices)
-    parser.add_argument("--check-for-version", choices=[2.03, 2.04], type=float)
-    parser.add_argument("--convert-to-version", choices=[2.03, 2.04], type=float)
+    parser.add_argument("--check-for-format", dest="chk_fmt", choices=fmt_choices)
+    parser.add_argument("--convert-to-format", dest="conv_fmt", choices=fmt_choices)
+    parser.add_argument(
+        "--check-for-version", dest="chk_ver", choices=[2.03, 2.04], type=float
+    )
+    parser.add_argument(
+        "--convert-to-version", dest="conv_ver", choices=[2.03, 2.04], type=float
+    )
     parser.add_argument("--dont-preserve-original", action="store_true")
     parser.add_argument("--keep-dds-file", action="store_true")
     args = parser.parse_args()
 
-    if not (args.convert_to_format or args.convert_to_version):
+    if not (args.conv_fmt or args.conv_ver):
         print("No conversion flags specified.\nExiting...")
-        exit(0)
-    if args.convert_to_version == 2.03 and args.convert_to_format in fmt_choices[6:-1]:
-        print(
-            f"{args.convert_to_format} is not compatible with FTEX {args.convert_to_version}."
-        )
-        exit(0)
-    if args.convert_to_format:
-        if (
-            sys.platform != "win32"
-            and subprocess.check_output(["whereis", "convert"]) == b"convert:\n"
-        ):
-            print(
-                "ImageMagick has not been found...\nPlease install it with your package manager."
-            )
-            exit(0)
+        exit(1)
+    if args.conv_ver == 2.03 and args.conv_fmt in fmt_choices[6:-1]:
+        print(f"{args.conv_fmt} is not compatible with FTEX {args.conv_ver}.")
+        exit(1)
+    if sys.platform != "win32":
+        if args.conv_fmt and check_output(["whereis", "convert"]) == b"convert:\n":
+            print("ImageMagick has not been found...")
+            print("Please install it with your package manager.")
+            exit(1)
+        if args.conv_fmt in fmt_choices[6:-1]:
+            print("Conversion to BC4, BC5, BC6 and BC7 is not supported on GNU/Linux.")
+            exit(1)
 
     kwargs = copy.copy(vars(args))
     del kwargs["path"]
 
     if os.path.isdir(args.path):
         for root, dirs, files in os.walk(args.path):
-            with Pool() as p:
-                p.map(partial(check_and_convert, filedir=root, **kwargs), files)
-            """
             for file in files:
-                check_and_convert(file, file_root=root, **kwargs)
-            """
+                if result := check_and_convert(file, filedir=root, **kwargs):
+                    print(result)
     else:
-        check_and_convert(args.path, **kwargs)
+        if result := check_and_convert(args.path, **kwargs):
+            print(result)
